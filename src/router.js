dd0ad9e807231159a901c4a905e355ad9f6f10d3
wanlianshow <111698490+wanlianshow@users.noreply.github.com>
2023-02-18 04:28:22 +0800

    测试中。。。

--- src/router.js
+++ src/router.js
@@ -6,7 +6,7 @@ const ENV = process.env.NODE_ENV
 router.get('/api/test', async (ctx, next) => {
   ctx.body = {
     errno: 0,
-    msg: `koa2 test jack , ENV: ${ENV}`
+    msg: `测试中。。。,ENV:${ENV}`
   }
 })
 
