90f4f3f84b766d6a4f4138f38d0f3ca62aa697ba
wanlianshow <111698490+wanlianshow@users.noreply.github.com>
2023-02-19 17:43:17 +0800

    测试中。。。

--- src/router.js
+++ src/router.js
@@ -6,7 +6,7 @@ const ENV = process.env.NODE_ENV
 router.get('/api/test', async (ctx, next) => {
   ctx.body = {
     errno: 0,
-    msg: `这是一条vscode编写的代码,ENV:${ENV}`
+    msg: `测试中。。。:${ENV}`
   }
 })
 
